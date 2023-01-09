4c809d6f86662b4da4e27c84d9d80ba04c2cac12
Oussama Cherni <59668656+Cherni-Oussama@users.noreply.github.com>
2023-01-09 19:16:19 +0100

    implement get employee by id logic (#5)
    Co-authored-by: Oussama Cherni <oussama.cherni@elyadata.com>

--- upstream-application/src/main/java/com/example/upstreamapplication/services/EmployeeService.java
+++ upstream-application/src/main/java/com/example/upstreamapplication/services/EmployeeService.java
@@ -3,8 +3,11 @@ package com.example.upstreamapplication.services;
 import com.example.upstreamapplication.models.Employee;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface EmployeeService {
 
     List<Employee> getAllEmployees();
+
+    Employee getEmployeeById(UUID employeeId);
 }
--- upstream-application/src/main/java/com/example/upstreamapplication/services/impl/EmployeeServiceImpl.java
+++ upstream-application/src/main/java/com/example/upstreamapplication/services/impl/EmployeeServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.upstreamapplication.services.impl;
 
+import com.example.upstreamapplication.exceptions.ItemNotFoundException;
 import com.example.upstreamapplication.models.Employee;
 import com.example.upstreamapplication.repositories.EmployeeRepository;
 import com.example.upstreamapplication.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @RequiredArgsConstructor
@@ -18,5 +21,11 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(UUID employeeId) {
+        return employeeRepository.findById(employeeId)
+                .orElseThrow(()-> new ItemNotFoundException("Employee Not Found", null, null, HttpStatus.NOT_FOUND));
     }
 }
