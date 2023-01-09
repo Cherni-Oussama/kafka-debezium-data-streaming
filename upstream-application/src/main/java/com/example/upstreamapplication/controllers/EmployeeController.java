4d8c08cbe9b80ef30ed75fbc1beec88c9404196a
Oussama Cherni <oussama.cherni@elyadata.com>
2023-01-09 19:15:22 +0100

    implement get employee by id logic

--- upstream-application/src/main/java/com/example/upstreamapplication/controllers/EmployeeController.java
+++ upstream-application/src/main/java/com/example/upstreamapplication/controllers/EmployeeController.java
@@ -6,10 +6,12 @@ import com.example.upstreamapplication.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/Employee")
@@ -22,4 +24,8 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("employeeId") UUID employeeId){
+        return ResponseEntity.ok(employeeService.getEmployeeById(employeeId));
+    }
 }
