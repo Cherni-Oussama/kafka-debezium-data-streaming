1fbb5f6e669a7985c17e543763c9ac4d3bdfd690
Oussama Cherni <oussama.cherni@elyadata.com>
2023-01-19 20:45:49 +0100

    using-naming-parameter-in-bash-script

--- kafka-connectors/src/scripts/start_connectors_script.bash
+++ kafka-connectors/src/scripts/start_connectors_script.bash
@@ -1,10 +1,22 @@
 /etc/confluent/docker/run &
 
-echo "Waiting for Kafka Connect to start listening on localhost ⏳"
+echo -e "\033[1;32m Getting all environment variables \033[m"
+
+declare -A hashmap
+for ARGUMENT in "$@"
+do
+   KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+   VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
+   hashmap["$KEY"]="$VALUE"
+   echo -e "\033[0;34m $KEY \033[m" = "$VALUE"
+done
+echo -e "\033[1;32m ${#hashmap[@]} parameters received \033[m"
+
+echo -e "\033[38;5;208m Waiting for Kafka Connect to start listening on localhost ⏳ \033[m"
 
 while :; do
 # shellcheck disable=SC1083
-curl_status=$(curl -s -o /dev/null -w %{http_code} http://localhost:"$8"/connectors)
+curl_status=$(curl -s -o /dev/null -w %{http_code} http://localhost:"${hashmap["CONNECT_PORT"]}"/connectors)
 # shellcheck disable=SC2046
 echo -e $(date) " Kafka Connect listener HTTP state: " $$curl_status " (waiting for 200)"
   if [ "$curl_status" -eq 200 ] ; then
@@ -14,21 +26,16 @@ echo -e $(date) " Kafka Connect listener HTTP state: " $$curl_status " (waiting 
   sleep 5
 done
 
-echo "Updating Source Connector with parameters ⏳"
+echo -e "\033[1;32m Verifying if all required parameters are available  \033[m"
 
+DEBEZIUM_JOB_PARAMETERS=("DBZ_CONNECTOR_CLASS" "DBZ_HOST" "DBZ_PORT" "DBZ_USER" "DBZ_PASSWORD" "DBZ_NAME" "TOPIC_PREFIX")
+for param in "${DEBEZIUM_JOB_PARAMETERS[@]}"
+do
+   sed -i "s/$param/${hashmap[$param]}/" src/configurations/postgres-debezium-config.json
+   echo "${hashmap[$param]}"
+done
 cat src/configurations/postgres-debezium-config.json
 
-sed -i 's/DBZ_CONNECTOR_CLASS/'"$1"'/' src/configurations/postgres-debezium-config.json
-sed -i 's/DBZ_HOST/'"$2"'/' src/configurations/postgres-debezium-config.json
-sed -i 's/DBZ_PORT/'"$3"'/' src/configurations/postgres-debezium-config.json
-sed -i 's/DBZ_USER/'"$4"'/' src/configurations/postgres-debezium-config.json
-sed -i 's/DBZ_PASSWORD/'"$5"'/' src/configurations/postgres-debezium-config.json
-sed -i 's/DBZ_NAME/'"$6"'/' src/configurations/postgres-debezium-config.json
-sed -i 's/TOPIC_PREFIX/'"$7"'/' src/configurations/postgres-debezium-config.json
-
-cat src/configurations/postgres-debezium-config.json
-
-echo -e "\n--\n+> Creating source connector"
-curl -i -X POST localhost:"$8"/connectors -H 'Content-Type: application/json' -d @src/configurations/postgres-debezium-config.json
-
+echo -e "\033[1;32m Creating source connector \033[m"
+curl -i -X POST localhost:"${hashmap["CONNECT_PORT"]}"/connectors -H 'Content-Type: application/json' -d @src/configurations/postgres-debezium-config.json
 sleep infinity
